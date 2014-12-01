883bfec20807e5b71411b1e0674a9cd448596535
Mannie Schumpert <mannieschumpert@gmail.com>
2014-12-01 18:55:01 -0500

    Initial plugin and class setup

---
+++ wp-relevance-query.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name:       WP Relevance Query
+ * Plugin URI:        http://mannieschumpert.com/plugins/wp-relevance-query/
+ * Description:       Extends WP_Query to allow for post queries sorted by relevance.
+ * Version:           0.0.1
+ * Author:            Mannie Schumpert
+ * Author URI:        http://mannieschumpert.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Initiate class
+ */
+class WP_Relevance_Query extends WP_Query {
+  
+	function __construct( $args = array() ) {
+
+		parent::__construct( $args );
+
+	}
+
+}
